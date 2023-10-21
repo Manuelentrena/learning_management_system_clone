13d046b0c459426364668d2e2999165192cc3137
EntrenaDev <manuel.entrena@gmail.com>
2023-10-21 20:49:12 +0200

    Added form to create new course

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
   return (
-    <div>
-      <p>Courses</p>
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
     </div>
   );
 };
