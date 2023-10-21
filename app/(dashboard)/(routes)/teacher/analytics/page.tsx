89a47cf3b4acfaa7972290bf5eb8c53198830691
EntrenaDev <manuel.entrena@gmail.com>
2023-10-21 20:16:46 +0200

    Added section pages in routes for teacher

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnalyticsPage = () => {
+  return (
+    <div>
+      <p>Analytics Page</p>
+    </div>
+  );
+};
+
+export default AnalyticsPage;
---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,9 @@
+const CoursesPage = () => {
+  return (
+    <div>
+      <p>Courses</p>
+    </div>
+  );
+};
+
+export default CoursesPage;
