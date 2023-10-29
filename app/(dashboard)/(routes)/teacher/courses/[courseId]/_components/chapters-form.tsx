44d5bbfe8f7bb4920227b63c4e79c822cacff070
EntrenaDev <manuel.entrena@gmail.com>
2023-10-29 17:14:47 +0100

    Added reorder chapter in course form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -20,8 +20,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
-
-// import { ChaptersList } from "./chapters-list";
+import { ChaptersList } from "./chapters-list";
 
 interface ChaptersFormProps {
   initialData: Course & { chapters: Chapter[] };
@@ -62,33 +61,33 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
     }
   };
 
-  // const onReorder = async (updateData: { id: string; position: number }[]) => {
-  //   try {
-  //     setIsUpdating(true);
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
 
-  //     await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
-  //       list: updateData,
-  //     });
-  //     toast.success("Chapters reordered");
-  //     router.refresh();
-  //   } catch {
-  //     toast.error("Something went wrong");
-  //   } finally {
-  //     setIsUpdating(false);
-  //   }
-  // };
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
 
-  // const onEdit = (id: string) => {
-  //   router.push(`/teacher/courses/${courseId}/chapters/${id}`);
-  // };
+  const onEdit = (id: string) => {
+    // router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
 
   return (
     <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
-      {/* {isUpdating && (
+      {isUpdating && (
         <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
           <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
         </div>
-      )} */}
+      )}
       <div className="font-medium flex items-center justify-between">
         Course chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -138,11 +137,11 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
           )}
         >
           {!initialData.chapters.length && "No chapters"}
-          {/* <ChaptersList
+          <ChaptersList
             onEdit={onEdit}
             onReorder={onReorder}
             items={initialData.chapters || []}
-          /> */}
+          />
         </div>
       )}
       {!isCreating && (
