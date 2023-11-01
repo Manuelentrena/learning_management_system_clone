e5f3078d76fea4db7da3a39c7ebbfe6cc89bcbaa
EntrenaDev <manuel.entrena@gmail.com>
2023-11-01 18:58:50 +0100

    Added chapter access form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,7 +9,7 @@ import { IconBadge } from "@/components/icon-badge";
 
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
-// import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 // import { ChapterVideoForm } from "./_components/chapter-video-form";
 // import { ChapterActions } from "./_components/chapter-actions";
 
@@ -104,11 +104,11 @@ const ChapterIdPage = async ({
                 <IconBadge icon={Eye} />
                 <h2 className="text-xl">Access Settings</h2>
               </div>
-              {/* <ChapterAccessForm
+              <ChapterAccessForm
                 initialData={chapter}
                 courseId={params.courseId}
                 chapterId={params.chapterId}
-              /> */}
+              />
             </div>
           </div>
           <div>
