2bc8769cf4ea960e83c11dbaaf83350e9b7093b0
EntrenaDev <manuel.entrena@gmail.com>
2023-11-02 19:32:39 +0100

    Fix bug in layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <Navbar />
       </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-        <Sidebar />d
+        <Sidebar />
       </div>
       <main className="md:pl-56 pt-[80px] h-full">{children}</main>
     </div>
