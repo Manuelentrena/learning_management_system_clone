c9c7c7d395ed2d37c8df81c40759349e92851d5b
EntrenaDev <manuel.entrena@gmail.com>
2023-11-09 19:13:50 +0100

    Added search component, in mobile and desktop

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,15 +5,22 @@ import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.startsWith("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
     <>
+      {isSearchPage && (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      )}
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isPlayerPage ? (
           <Link href="/">
