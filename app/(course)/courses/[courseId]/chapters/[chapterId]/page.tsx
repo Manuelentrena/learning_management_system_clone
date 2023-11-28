edbfc8161dd75eeefc2d6927c36e6bf943748c77
EntrenaDev <manuel.entrena@gmail.com>
2023-11-28 12:09:21 +0100

    Added button to mark how completed chapter

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -9,7 +9,7 @@ import { Preview } from "@/components/preview";
 
 import { VideoPlayer } from "./_components/video-player";
 import { CourseEnrollButton } from "./_components/course-enroll-button";
-// import { CourseProgressButton } from "./_components/course-progress-button";
+import { CourseProgressButton } from "./_components/course-progress-button";
 
 const ChapterIdPage = async ({
   params,
@@ -70,13 +70,12 @@ const ChapterIdPage = async ({
           <div className="p-4 flex flex-col md:flex-row items-center justify-between">
             <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
             {purchase ? (
-              /* <CourseProgressButton
+              <CourseProgressButton
                 chapterId={params.chapterId}
                 courseId={params.courseId}
                 nextChapterId={nextChapter?.id}
                 isCompleted={!!userProgress?.isCompleted}
-              /> */
-              <></>
+              />
             ) : (
               <CourseEnrollButton
                 courseId={params.courseId}
