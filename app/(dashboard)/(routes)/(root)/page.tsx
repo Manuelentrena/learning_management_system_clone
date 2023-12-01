c521d67ddb4bf98835dd0a15fbe192b6a7e2e796
EntrenaDev <manuel.entrena@gmail.com>
2023-12-01 11:45:12 +0100

    Added dashboard with courses completed and progress

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+
+import { CoursesList } from "@/components/courses-list";
+import { CheckCircle, Clock } from "lucide-react";
+import InfoCard from "./_components/info-card";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
+
+  return (
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numbersOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numbersOfItems={completedCourses.length}
+          variant="success"
+        />
+      </div>
+      <CoursesList items={[...completedCourses, ...coursesInProgress]} />
+    </div>
+  );
+}
